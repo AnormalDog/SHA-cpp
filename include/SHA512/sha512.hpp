ecde7527e9a06979ab897183ebbf2eb4ed9450ff
AnormalDog <anormaldog100@gmail.com>
2025-04-06 11:55:56 +0100

    first branch SHA512 commit

---
+++ include/SHA512/sha512.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file sha512.hpp
+ * @author AnormalDog
+ * @brief 
+ * @version 0.1
+ * @date 2025-04-06
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#ifndef SHA512_HPP_
+#define SHA512_HPP_
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+namespace sha512 {
+  namespace constants {
+    extern const uint64_t k_constants[80];
+  }
+}
+
+#endif
